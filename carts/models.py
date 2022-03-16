ca08ba3f69651f164b011e737771e60e642df8bf
천지은 <jieun@MacBook-Pro-5.local>
2022-03-16 17:47:55 +0900

    Modify: modify modeling

--- carts/models.py
+++ carts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-class Cart(models.Model):
+from utils.time_stamp_model import TimeStampModel
+class Cart(TimeStampModel):
     user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
+    series  = models.ForeignKey('products.Series', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'carts'
