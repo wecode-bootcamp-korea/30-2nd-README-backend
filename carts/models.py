8fbf66713e1a13f4530aa0be7e14f49ac96844e7
Kimgwangil <kwangil0803@gmail.com>
2022-03-21 14:00:41 +0900

    Fix: order/cart/user models

--- carts/models.py
+++ carts/models.py
@@ -4,7 +4,14 @@ from utils.time_stamp_model import TimeStampModel
 class Cart(TimeStampModel):
     user    = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    series  = models.ForeignKey('products.Series', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'carts'
+
+
+class CartSeries(TimeStampModel):
+    cart   = models.ForeignKey('Cart', on_delete=models.CASCADE)
+    series = models.ForeignKey('products.Series', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'cart_series'
