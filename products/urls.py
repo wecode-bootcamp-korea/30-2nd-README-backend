b7b98a633bfa7a6c6790e30fe9058cc21a8d43af
천지은 <jieun@MacBook-Pro-5.local>
2022-03-23 13:59:14 +0900

    Modify: feature/detail

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from products.views import ProductListView, ProductBestsellerView, ProductReadmePickView, ProductRecommendView
+from products.views import ProductListView, ProductBestsellerView, ProductReadmePickView, ProductRecommendView, ProductDetailView, SeriesListView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('/bestseller', ProductBestsellerView.as_view()),
     path('/recommend', ProductRecommendView.as_view()),
-    path('/readmepick', ProductReadmePickView.as_view())
+    path('/readmepick', ProductReadmePickView.as_view()),
+    path('/details/<int:product_id>', ProductDetailView.as_view()),
+    path('/books/<int:product_id>', SeriesListView.as_view())
 ]
