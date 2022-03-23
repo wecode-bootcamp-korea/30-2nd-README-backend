0375fc0ab5bed71fe6604701d377bdf5a7e5f01c
Kimgwangil <kwangil0803@gmail.com>
2022-03-23 11:25:07 +0900

    Add: Order

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,134 @@
-from django.test import TestCase
+from django.http import response
+import jwt, json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from my_settings     import SECRET_KEY, ALGORITHM
+from .models         import Order, OrderSeries
+from users.models         import User, Gender
+from products.models import Product, Author, Publisher, Translator, Series, Category
+from carts.models    import Cart, CartSeries
+
+class OrderTest(TestCase):
+    def setUp(self):
+        Gender.objects.create(
+            id  = 1,
+            sex = 'male'
+        )
+        User.objects.create(
+            id        = 1,
+            kakao_id  = 12345,
+            nickname  = 'gwang',
+            point     = 50,
+            gender_id = 1
+        )
+
+        Author.objects.create(
+            id   = 1,
+            name = '광작'
+        )
+
+        Translator.objects.create(
+            id   = 1,
+            name = '팡'
+        )
+
+        Publisher.objects.create(
+            id   = 1,
+            name = '오르골출판'
+        )
+
+        Category.objects.create(
+            id   = 1,
+            name = '자서전'
+        )
+        
+        Product.objects.create(
+            id            = 1,
+            name          = '광',
+            description   = '광일',
+            author_id     = 1,
+            translator_id = 1,
+            publisher_id  = 1,
+            category_id   = 1
+        )
+        
+        Series.objects.create(
+            id           = 1,
+            name         = '광일',
+            price        = 1000,
+            sequence     = 1,
+            published_at = '2020-02-02',
+            product_id   = 1
+        )
+
+        Series.objects.create(
+            id           = 2,
+            name         = '광이',
+            price        = 500,
+            sequence     = 2,
+            published_at = '2020-02-04',
+            product_id   = 1
+        )
+
+        Cart.objects.create(
+            id         = 1,
+            user_id    = 1,
+            product_id = 1
+        )
+        
+        CartSeries.objects.create(
+            id        = 1,
+            cart_id   = 1,
+            series_id = 1
+        )
+        
+        CartSeries.objects.create(
+            id        = 2,
+            cart_id   = 1,
+            series_id = 2
+        )
+
+    def tearDown(self):
+        Gender.objects.all().delete()
+        User.objects.all().delete()
+        Author.objects.all().delete()
+        Translator.objects.all().delete()
+        Publisher.objects.all().delete()
+        Product.objects.all().delete()
+        Series.objects.all().delete()
+        Cart.objects.all().delete()
+        CartSeries.objects.all().delete()
+
+    def test_order_success(self):
+        client = Client()
+
+        access_token = jwt.encode({'id':12345}, SECRET_KEY, ALGORITHM)
+        headers      = {'HTTP_Authorization' : access_token}
+
+        order = {
+            'product_id' : 1,
+            'series_ids' : [1,2]
+        }
+
+        response = client.post('/orders', json.dumps(order), 
+                            content_type='application/json', **headers)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_order_404_error_DOES_NOT_EXIST_CART(self):
+        client = Client()
+
+        access_token = jwt.encode({'id':12345}, SECRET_KEY, ALGORITHM)
+        headers      = {'HTTP_Authorization' : access_token}
+
+        order = {
+            'product_id' : 1,
+            'series_ids' : [3],
+        }
+
+        response = client.post('/orders', json.dumps(order), 
+                            content_type='application/json', **headers)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(), {'message' : 'CART_DOES_NOT_EXIST'})
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,61 @@
 import json, uuid
 
-from django.http import JsonResponse, HttpResponse
+from django.http  import JsonResponse, HttpResponse
 from django.views import View
+from django.db    import transaction
 
-from users.decorators import *
-from .models import *
-from products.models import *
-from users.models import *
-from carts.models import *
+from users.decorators import login_decorator
+from .models          import Order, OrderSeries
+from carts.models     import Cart, CartSeries
+
+class OrderView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            user = request.user
+
+            data = json.loads(request.body)
+
+            product_id = data['product_id']
+            series_ids = data['series_ids']
+
+            cart_series  = CartSeries.objects.select_related('cart', 'series')\
+                                             .filter(cart__product_id = product_id, series_id__in = series_ids)
+                                             
+            with transaction.atomic():
+                if not cart_series.exists():
+                    return JsonResponse({'message' : 'CART_DOES_NOT_EXIST'}, status = 404)
+
+                self.create_order_and_orderseries(user, product_id, series_ids)
+
+                cart_series.delete() 
+
+                self.delete_cartseries(product_id)
+                
+            return HttpResponse(status = 201)
+        
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+            
+    def create_order_and_orderseries(self, user, product_id, series_ids):
+        order, is_created = Order.objects.get_or_create(
+            user_id      = user.id,
+            product_id   = product_id,
+            defaults     = {
+                'order_number' : uuid.uuid4(),
+            }
+        )
+
+        order_series = [
+            OrderSeries(
+            order_id  = order.id,
+            series_id = series
+            )
+            for series in series_ids if CartSeries.objects.filter(series_id = series).exists()
+        ]
+
+        OrderSeries.objects.bulk_create(order_series)
+
+    def delete_cartseries(self, product_id):
+        if not CartSeries.objects.filter(cart__product_id = product_id):
+            Cart.objects.filter(product_id = product_id).delete()
